e98b3f0cf681c47a1b356d34a50b542643baaac0
DatalinkNetworks <105780511+DatalinkNetworks@users.noreply.github.com>
2024-07-31 20:39:16 -0700

    Update init-mongo.js

--- init-mongo.js
+++ init-mongo.js
@@ -1,2 +1,2 @@
-db.getSiblingDB("${MONGO_DBNAME}").createUser({user: "${MONGO_USER}", pwd: "${MONGO_PASS}", roles: [{role: "dbOwner", db: "${MONGO_DBNAME}"}]});
-db.getSiblingDB("${MONGO_DBNAME}_stat").createUser({user: "${MONGO_USER}", pwd: "${MONGO_PASS}", roles: [{role: "dbOwner", db: "${MONGO_DBNAME}_stat"}]});
+db.getSiblingDB(process.env.MONGO_DBNAME).createUser({user: process.env.MONGO_USER, pwd: process.env.MONGO_PASS, roles: [{role: "dbOwner", db: process.env.MONGO_DBNAME}]});
+db.getSiblingDB(process.env.MONGO_DBNAME + "_stat").createUser({user: process.env.MONGO_USER, pwd: process.env.MONGO_PASS, roles: [{role: "dbOwner", db: process.env.MONGO_DBNAME+ "_stat"}]});
